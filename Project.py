283abd713bf00bc7c21e1cb4574886699c694193
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-19 00:19:28 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -25,6 +25,15 @@ st.markdown("""
 
 st.write(dt.head(1))
 
+# เพิ่มตัวเลือกสำหรับผู้ใช้ในการกรองข้อมูล
+gender_option = st.sidebar.selectbox("เลือกเพศ", ["ชาย", "หญิง"])
+
+filtered_df = data[data["Sex"] == gender_option]
+
+# แสดงจำนวนผู้เสียชีวิตและบาดเจ็บแยกตามเพศ
+col1, col2 = st.columns(2)
+
+
 #st.write
 NumM=dt[dt['Sex']=='ชาย'].count()
 NumF=dt[dt['Sex']=='หญิง'].count()
