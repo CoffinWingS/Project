d20fc36b37372d251141938391602fb6a151282e
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-19 00:17:26 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -7,11 +7,12 @@ st.header("สถิติการเกิดอุบัติเหตุใ
 col1,col2=st.columns(2)
 
 with col1:
-st.subheader("จำนวนผู้เสียชีวิต")
-st.write("2,5600")
+    st.subheader("จำนวนผู้เสียชีวิต")
+    st.write("2,5600")
 with col2:
-st.subheader("จำนวนผู้เสียชีวิต")
-st.write("2,5600")
+    st.subheader("จำนวนผู้เสียชีวิต")
+    st.write("2,5600")
+
 
 dt=pd.read_excel('data/DATA.xlsx')
 
