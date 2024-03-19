a75034979c4dffd08cdf6d5b2ef45773d39bd401
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2024-03-19 23:06:27 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -7,10 +7,10 @@ st.header("สถิติการเกิดอุบัติเหตุใ
 
 col1,col2=st.columns(2)
 
- with col3:
+with col1:
     st.subheader("จำนวนผู้เสียชีวิต")
     st.write("2,5600")
- with col4:
+with col2:
     st.subheader("จำนวนผู้เสียชีวิต")
     st.write("2,5600")
 
